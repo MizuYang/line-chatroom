d63339c8274453f8b9c44186eb9359e48679fd00
Mizu <psdk55052004@gmail.com>
2023-05-16 14:59:50 +0800

    新增 vuex

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import 'bootstrap'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import Vuex from 'vuex'
+import store from './vuex/index'
 
 import App from './App.vue'
 import router from './router'
@@ -10,4 +12,6 @@ const app = createApp(App)
 
 app.use(router)
 app.use(VueAxios, axios)
+app.use(Vuex)
+app.use(store)
 app.mount('#app')
---
+++ src/vuex/index.js
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex'
+// import axios from 'axios'
+
+// 其它store
+
+export default createStore({
+  strict: false, // 嚴謹模式
+  state: {
+  },
+  actions: {
+  },
+  mutations: {
+  },
+  getters: {
+  },
+  modules: {
+  }
+})
