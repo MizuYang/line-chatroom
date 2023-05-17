b63ce1962d6ae06f8ec855cec32c884985591f1d
Mizu <psdk55052004@gmail.com>
2023-05-17 10:58:18 +0800

    新增 討論區列表

---
+++ src/components/chatroomList/TopicArea.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="d-flex">
+    <!-- 群組圖 -->
+    <span class="fw-bold-7 rounded-pill my-auto me-5 px-6 py-7" style="background-color: #ffcd83">
+      {{ topic.name.slice(0, 2) }}
+    </span>
+    <div style="max-width:230px;">
+      <p class="d-flex">
+        <!-- 群組名稱 -->
+        <span class="text-cut fw-bold-7 mb-2 me-1">{{ topic.name }}</span>
+        ({{ topic.peopleNumber }})
+      </p>
+      <!-- 最新一則訊息 -->
+      <p class="text-13 text-808080 text-start text-cut-line2" style="max-height:40px;">
+        {{ topic.messages }}
+      </p>
+    </div>
+    <div class="d-flex flex-column ms-auto me-7">
+      <time class="text-13 text-gray mb-2">{{ getTime(topic.newestTime) }}</time>
+      <!-- 未讀數量 -->
+      <span class="badge rounded-pill bg-danger text-12 text-center fw-bold-5 mx-auto px-2"
+            :style="`width:${topic.unreadCount>999?38:27}px;`" v-if="topic.unreadCount">
+        {{ topic.unreadCount>999?'999+':topic.unreadCount }}
+      </span>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: ['topic'],
+
+  data () {
+    return {}
+  },
+
+  methods: {
+    getTime (time) {
+      // 最新一筆訊息的時間不是今天: 顯示日期
+
+      const today = new Date().toISOString().slice(0, 10) // 2023-05-16
+      const yesterday = new Date(new Date().getTime() - 86400000).toISOString().slice(0, 10) // 2023-05-16
+      const msgDate = time.slice(0, 10) // 2023-05-16
+      const timeSlice = time.slice(11) // 08:32:00
+
+      // 最新一筆訊息的時間是今天: 顯示時間
+      if (today === msgDate) {
+        return timeSlice.slice(0, 5) // 08:32
+      } else if (yesterday === msgDate) {
+        // 最新一筆訊息的時間是昨天: 顯示文字"昨天"
+        return '昨天'
+      } else {
+        const date = msgDate.slice(5).split('-').join('/') // 05/03
+        return date
+      }
+    }
+  },
+
+  mounted () {}
+}
+</script>
+
+<style lang='scss' scope></style>
---
+++ src/views/ChatRoomList.vue
@@ -0,0 +1,71 @@
+<template>
+  <header>
+    <NavBar></NavBar>
+  </header>
+
+  <nav class="position-fixed top-0 w-100" style="background-color:#CBE6FF;margin-top:96px;padding:8px 0;">
+    <input type="search" class="form-control raduis-5 placeholder-ps30 mx-auto" v-model="searchTxt" @focus="isFocus=true" @blur="isFocus=false" style="width:350px;">
+
+    <div class="position-absolute d-flex align-items-center" style="top:50%;left:32px;transform:translateY(-50%)" v-if="!isFocus&&!searchTxt">
+      <img src='@/assets/images/chatroomList/搜尋.svg' alt='搜尋'>
+      <span class="text-808080 fw-bold-5 ms-3">搜尋</span>
+    </div>
+  </nav>
+
+  <ul class="ms-8" style="margin-top:170px;">
+    <li class="mb-11" v-for="topic in cahtroomList" :key="`chatroom-${topic.subjectId}`">
+      <button type="button" class="w-100 btn p-0" @click="enterChatroom(topic)">
+        <!-- 討論區話題元件 -->
+        <TopicArea :topic="topic"></TopicArea>
+      </button>
+    </li>
+  </ul>
+
+</template>
+
+<script>
+import NavBar from '@/components/layout/navbar/NavBar.vue'
+import TopicArea from '@/components/chatroomList/TopicArea.vue'
+export default {
+  components: {
+    NavBar,
+    TopicArea
+  },
+
+  data () {
+    return {
+      cahtroomList: [],
+      isFocus: false,
+      searchTxt: ''
+    }
+  },
+
+  methods: {
+    getChatroomList () {
+      this.$http.get('api/chatroomList.json')
+        .then(res => {
+          console.log(res.data)
+          this.cahtroomList = res.data
+          //! 排序應該是  新 -> 舊
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    },
+    enterChatroom (topic) {
+      console.log('進入聊天室', topic)
+    }
+  },
+
+  mounted () {
+    this.getChatroomList()
+  }
+
+}
+</script>
+
+<style lang='scss' scope>
+.placeholder-ps30::placeholder {
+  padding-left: 30px;
+}
+</style>
